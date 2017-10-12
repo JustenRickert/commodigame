d6e49ac4ff7787bdbaaa7dcb7719dcb021ab2739
Justen Rickert <justenrickert@gmail.com>
2017-10-11 23:42:33 -0500

    More refactoring. The data structures are correct now.

--- src/commgame/commodities.cljs
+++ src/commgame/commodities.cljs
@@ -4,87 +4,77 @@
 ;; words to spell a lot!
 
 (defonce b-comm-data-before-id
-  [{:title "sand"  :price 0.25}
-   {:title "sugar" :price 0.50}
-   {:title "hops"  :price 0.50}
-   {:title "water" :price 0.10}
-   {:title "fruit" :price 1.00}])
+  {"sand"  {:price 0.25}
+   "sugar" {:price 0.50}
+   "hops"  {:price 0.50}
+   "water" {:price 0.10}
+   "fruit" {:price 1.00}})
 
 (defonce c-comm-data-before-id
-  [; single input combinational commodities
-   {:title "glass"
-    :price 2.15
-    :input [{:title "sand"  :quan 5}]
+  {"glass" ; single input combinational commodities
+   {:price       2.15
+    :input       [{:title "sand" :quan 5}]
     :output-quan 3}
 
-   {:title "ice"
-    :price 0.75
-    :input [{:title "water" :quan 5}]
+   "ice"
+   {:price       0.75
+    :input       [{:title "water" :quan 5}]
     :output-quan 6}
 
-   {:title "crushed fruit"
-    :price 1.10
-    :input [{:title "fruit" :quan 3}]
+   "crushed fruit"
+   {:price       1.10
+    :input       [{:title "fruit" :quan 3}]
     :output-quan 4}
 
-                                        ; multiple input combinational commodities
-   {:title "ice water"
-    :price 0.90
-    :input [{:title "water" :quan 4}
-            {:title "ice"   :quan 1}]
+   "ice water" ; multiple input combinational commodities
+   {:price       0.90
+    :input       [{:title "water" :quan 4}
+                  {:title "ice"   :quan 1}]
     :output-quan 5}
 
-   {:title "fruit drink"
-    :price 2.15
-    :input [{:title "water" :quan 2}
-            {:title "sugar" :quan 2}
-            {:title "fruit" :quan 1}]
+   "fruit drink"
+   {:price       2.15
+    :input       [{:title "water" :quan 2}
+                  {:title "sugar" :quan 2}
+                  {:title "fruit" :quan 1}]
     :output-quan 5}
 
-   {:title "cold fruit drink"
-    :price 3.10
-    :input [{:title "ice"         :quan 1}
-            {:title "fruit drink" :quan 2}]
+   "cold fruit drink"
+   {:price       3.10
+    :input       [{:title "ice"         :quan 1}
+                  {:title "fruit drink" :quan 2}]
     :output-quan 2}
 
-   {:title "beer"
-    :price 4.00
-    :input [{:title "hops"  :quan 1}
-            {:title "water" :quan 2}
-            {:title "glass" :quan 3}]
+   "beer"
+   {:price      4.00
+    :input      [{:title "hops"  :quan 1}
+                 {:title "water" :quan 2}
+                 {:title "glass" :quan 3}]
     :ouput-quan 5}
 
-   {:title "lager"
-    :price 3.95
-    :input [{:title "hops"  :quan 1}
-            {:title "water" :quan 2}
-            {:title "ice"   :quan 1}
-            {:title "glass" :quan 4}]
+   "lager"
+   {:price       3.95
+    :input       [{:title "hops"  :quan 1}
+                  {:title "water" :quan 2}
+                  {:title "ice"   :quan 1}
+                  {:title "glass" :quan 4}]
     :output-quan 6}
 
-   {:title "radler"
-    :price 5.35
-    :input [{:title "beer"             :quan 1}
-            {:title "cold fruit drink" :quan 1}]
-    :output-quan 2}])
+   "radler"
+   {:price       5.35
+    :input       [{:title "beer"             :quan 1}
+                  {:title "cold fruit drink" :quan 1}]
+    :output-quan 2}})
 
 ;; Want to add a random id to each one because it helps react do things quickly.
 ;; The console also complains otherwise.
-
 (defonce b-comm-data
   ^{:doc "Basic commodities data. Commodities that cannot be created through
           combination"}
-  (mapv #(assoc % :key (random-uuid)) b-comm-data-before-id))
+  (into {} (for [[k v] b-comm-data-before-id] {k (assoc v :key (random-uuid))})))
 
 (defonce c-comm-data
   ^{:doc "Combinational commodities data. Commodities that can be created
           through combination of any basic commodities or other combinational
           commodities"}
-  (mapv #(assoc % :key (random-uuid)) c-comm-data-before-id))
-
-(defn comb-inputs-of-title [title]
-  (loop [c-comm c-comm-data]
-    (let [inputs (:input (first c-comm))]
-      (cond (= title (:title (first c-comm))) inputs
-            (empty? c-comm) nil
-            :else (recur (rest c-comm))))))
+  (into {} (for [[k v] c-comm-data-before-id] {k (assoc v :key (random-uuid))})))
--- src/commgame/render.cljs
+++ src/commgame/render.cljs
@@ -3,59 +3,62 @@
             [commgame.commodities :as comm]
             [goog.string :as string]))
 
-(defn comm-li
-  [comm]
-  (js/console.log "comm-li rerender")
-  [:div.comm
-   [:div (:title comm) [:br] (:quan comm)]])
+(defn comm-li [[title comm]]
+  (js/console.log "comm-li" title "rerender")
+  [:div.comm title [:br] (:quan comm)])
 
-(defn buy-comm-button [comm]
-  (js/console.log "buy-comm-button rerender")
+(defn buy-comm-button [[title comm]]
+  (js/console.log "comm-button" title "rerender")
   [:div.comm
-   [:div (:title comm) [:br] "$" (string/format "%.2f" (:price comm))]
-   [:button {:on-click #(game/user-buy-one-comm (:title comm))}
+   [:div title [:br] "$" (string/format "%.2f" (:price comm))]
+   [:button {:on-click #(game/user-buy-one-comm title)}
     "💲"]])
 
 (defn combine-comm-button
-  [c-comm]
-  (js/console.log "c-comm-button rerender")
-  (let [inputs (comm/comb-inputs-of-title (:title c-comm))]
+  [[title comm]]
+  (js/console.log "c-comm-button" title "rerender")
+  (let [inputs (:input comm)]
     [:div.comm
-     [:span (:output-quan c-comm) " " (:title c-comm) [:br] "@" [:br]
+     [:span (:output-quan comm) " " title [:br] "@" [:br]
       (for [input inputs]
         (str " " (:quan input) " " (:title input)))]
      [:br]
-     [:button {:on-click #(game/user-combine-for-comm c-comm)}
+     [:button {:on-click #(game/user-combine-for-comm title)}
       "🔧"]]))
 
 (defn comb-comm-market []
   ;; (let [titles-with-combs (set (mapv #(:title %) comm/c-comm-data))]
   [:div [:h3 "Combination"]
    [:div.user-comm
-      ;; comp is component. A react component.
-    (let [comp (doall (remove nil? (for [c-comm comm/c-comm-data]
-                                     (when (game/c-comm-combinable? (:title c-comm))
-                                       [combine-comm-button c-comm]))))]
-      (if (empty? comp)
+    (let [component (doall
+                     (remove nil?
+                             (for [[title comm] comm/c-comm-data]
+                               (when (game/c-comm-combinable? title)
+                                 ^{:key (str "comb btn" title)}
+                                 [combine-comm-button [title comm]]))))]
+      (if (empty? component)
         [:span "No possible combinations yet!"]
-        comp))]])
+        component))]])
 
 (defn basic-comm-market []
   [:div [:h3 "Type B Commodities Market"]
    [:div.user-comm
-    (for [comm comm/b-comm-data]
-      [buy-comm-button comm])]])
+    (for [[title comm] comm/b-comm-data]
+      ^{:key (str "btn" title)}
+      [buy-comm-button [title comm]])]])
 
 (defn page []
   [:div.page
-   [:div.money "$" (string/format "%.2f" (get-in @game/app-state [:user :money]))]
+   [:div.money "$" (string/format "%.2f" (:money @game/user-state))]
    [basic-comm-market]
    [comb-comm-market]
    [:div [:h3 "User commodities"]
     [:div.user-comm
-     (let [comp (doall (remove nil? (for [c (game/user-commodities)]
-                                      (when (not= 0 (:quan c))
-                                        [comm-li c]))))]
-       (if (empty? comp)
+     (let [component (doall
+                      (remove nil? (for [[title comm] (:comm @game/user-state)]
+                                     (when (not= 0 (:quan comm))
+                                       ^{:key (str "li" title)}
+                                       [comm-li [title comm]]))))]
+       (if (empty? component)
          [:span "No commodities yet!"]
-         comp))]]])
+         component))]]])
